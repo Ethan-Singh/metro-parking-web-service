de6b35c1eaa3a49def27197addae3d2df2d1eb86
Ethan-Singh <100287262+Ethan-Singh@users.noreply.github.com>
2026-05-30 03:27:19 +1000

    Add springdoc and slight refactoring

--- src/main/java/com/example/metro_parking_web_service/server/HelloController.java
+++ src/main/java/com/example/metro_parking_web_service/server/HelloController.java
@@ -1,5 +1,5 @@
 /* (C)2026 */
-package com.example.metro_parking_web_service.controller;
+package com.example.metro_parking_web_service.server;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
