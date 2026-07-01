1d94e0c231ee268333b175c4d0a43c5f7d78ea6a
Ethan-Singh <100287262+Ethan-Singh@users.noreply.github.com>
2026-07-01 17:23:58 +1000

    feature(test): Update ParkingSecurityPropertiesTest to SecurityPropertiesTest

--- src/test/java/com/example/metro_parking_web_service/parking/analytics/config/SecurityPropertiesTest.java
+++ src/test/java/com/example/metro_parking_web_service/parking/analytics/config/SecurityPropertiesTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
-class ParkingSecurityPropertiesTest {
+class SecurityPropertiesTest {
 
     @Autowired private SecurityProperties props;
 
